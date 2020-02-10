4f0079020ac0fb9fd0b965a2305e4a82ab4ee348
Lilia Podolska <pervak.lilia@gmail.com>
2020-02-10 23:15:35 +0200

    first issue

---
+++ main.js
@@ -0,0 +1,6 @@
+let first = 127;
+let second = 204;
+let third = 192;
+let random = Math.random();
+
+console.log ('background-color: rgba(' + String(first) + ', ' + String(second) + ', ' + String(third) + ', ' + String(random) + ')');
